32837f48a7965fe5f14b3324d4e85af6d04b2215
Bernie Lorbis <bernielorbis@Bernies-MacBook-Pro.local>
2025-01-19 20:38:46 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import router from "@/routes";
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
